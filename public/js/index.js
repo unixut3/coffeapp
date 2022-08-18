c9f5b94b9d3d61ca681e93971984e15c2783f0ca
unixut3 <dlwodusrj@naver.com>
2022-08-18 15:58:13 +0900

    aassd

--- public/js/index.js
+++ public/js/index.js
@@ -19,12 +19,53 @@ import {
       //doc에 있는 목록 가져오기
       querySnapshot.forEach((doc) => {
         const task = doc.data();
+        var tableHTML = "";
+        var cnt = task.beenList == null ? 0 : task.beenList.length;
+
+        for (let i = 0; i < cnt; i++) {
+          tableHTML += `
+          <tr>
+            <td>${task.beenList[i].name}</td>
+            <td style="text-align: right;">${task.beenList[i].weight}</td>
+            <td style="text-align: right;">${task.beenList[i].price}</td>
+          </tr>
+          `
+        }
   
         tasksContainer.innerHTML += `
           <div class="card card-body mt-2 border-primary">
-            <h3 class="h5">${task.rosteryName}</h3>
-            <p>${task.location}</p>
-            <image src="${task.imageUrl}" class="thumbnail"></image>
+          <table>
+            <tr style="text-align: left; vertical-align: top;">
+              <td width="200px;">
+                <image src="${task.imageUrl}" class="thumbnail"></image>
+              </td>
+              <td width="250px;">
+                <h3 class="h5">로스터리명 : ${task.rosteryName}</h3>
+                <p>위치 : ${task.location}</p>
+                <p>인스타 : ${task.instaId}</p>
+                <p>스토어 : ${task.store}</p>
+                <p>이달의원두 여부 : ${task.monthlyYn}</p>
+                <p>설명 : ${task.description}</p>
+              </td>
+              <td width="300px;">
+                <h3 class="h5" style="text-align: center;">원두목록</h3>
+                <div>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th style = "width:200px; text-align: center;">원두명</th>
+                        <th style = "width:100px; text-align: center;">그람</th>
+                        <th style = "width:100px; text-align: center;">가격</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      ${tableHTML}
+                    </tbody>
+                  </table>
+                </div>
+              </td>
+            </tr>
+          </table>
           <div>
             <button class="btn btn-primary btn-delete" data-id="${doc.id}">
               🗑 Delete
